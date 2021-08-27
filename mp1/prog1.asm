88fc98779afa359fe0e7b4ac079f5f5f9d0a2ef8
Sheng-Wei Huang <swhuang3@illinois.edu>
2021-08-27 11:13:10 -0500

    Finish lab2

--- mp1/prog1.asm
+++ mp1/prog1.asm
@@ -104,9 +104,152 @@ PRINT_HIST
 ; for your implementation, list registers used in this part of the code,
 ; and provide sufficient comments
 
+; R0: char
+; R1: counter
+; R2: tmp
+; R3: parameter
+	LD R1, ZERO
+
+PRINT_HIST_LOOP
+	ADD R2, R1, #-16
+	ADD R2, R2, #-11
+	BRzp DONE
+
+	LD R0, ASCII_AT
+	ADD R0, R0, R1
+	OUT
+
+	LD R0, ASCII_SPACE
+	OUT
+
+	LD R0, HIST_ADDR
+	ADD R0, R0, R1
+	LDR R3, R0, #0
+
+	JSR  PRINT_NUMBER
+ 
+
+	LD R0, ASCII_NL
+	OUT
+
+
+	ADD R1, R1, #1
+	BRnzp PRINT_HIST_LOOP
+
+
+	
+
+
+
+
+
 
 
 DONE	HALT			; done
+
+
+
+; Function PRINT_NUMBER
+; Registers:
+;   R3: input
+;   R4: digitCounter
+;   R5: digit
+;   R6: bitCounter
+;   R0: tmp
+PRINT_NUMBER
+    ST R0, R0_SAVE
+    ST R1, R1_SAVE
+    ST R2, R2_SAVE
+    ST R3, R3_SAVE
+    ST R4, R4_SAVE
+    ST R5, R5_SAVE
+    ST R6, R6_SAVE
+    ST R7, R7_SAVE
+
+
+    LD  R4, ZERO
+    ADD R4, R4, #4
+
+DIGIT_LOOP
+    AND R4, R4, R4
+    BRnz PRINT_NUMBER_END
+
+
+    LD  R5, ZERO
+    LD  R6, ZERO
+    ADD R6, R6, #4
+
+BIT_LOOP
+    AND R6, R6, R6
+    BRnz DISPLAY_DIGIT
+
+
+    ADD R5, R5, R5
+
+    AND R3, R3, R3
+    BRn NUM_MSB_1
+    BRnzp NUM_MSB_0
+
+NUM_MSB_1
+    ADD R5, R5, #1
+    BRnzp NUM_MSB_END
+
+NUM_MSB_0
+    ADD R5, R5, #0
+    BRnzp NUM_MSB_END
+
+NUM_MSB_END
+
+    ADD R3, R3, R3
+
+
+    ADD R6, R6, #-1
+    BRnzp BIT_LOOP
+
+
+
+
+DISPLAY_DIGIT
+    LD R0, ZERO
+    ADD R0, R0, R5
+
+    ADD R0, R0, #-9
+    BRnz DIGIT_LE_9
+    BRnzp DIGIT_GT_9
+
+DIGIT_LE_9
+    LD R0, ASCII_ZERO
+    ADD R5, R5, R0
+
+    BRnzp DIGIT_END
+
+DIGIT_GT_9
+    ADD R5, R5, #-10
+
+    LD R0, ASCII_A_CAP
+    ADD R5, R5, R0
+
+    BRnzp DIGIT_END
+
+DIGIT_END
+    LD R0, ZERO
+    ADD R0, R0, R5
+    OUT	
+
+
+    ADD R4, R4, #-1
+    BRnzp DIGIT_LOOP
+
+PRINT_NUMBER_END
+    LD R0, R0_SAVE
+    LD R1, R1_SAVE
+    LD R2, R2_SAVE
+    LD R3, R3_SAVE
+    LD R4, R4_SAVE
+    LD R5, R5_SAVE
+    LD R6, R6_SAVE
+    LD R7, R7_SAVE
+    RET
 
 
 ; the data needed by the program
@@ -116,6 +259,23 @@ AT_MIN_Z	.FILL xFFE6	; the difference between ASCII '@' and 'Z'
 AT_MIN_BQ	.FILL xFFE0	; the difference between ASCII '@' and '`'
 HIST_ADDR	.FILL x3F00     ; histogram starting address
 STR_START	.FILL x4000	; string starting address
+
+
+ZERO        .FILL x0000
+ASCII_ZERO  .FILL x0030
+ASCII_A_CAP .FILL x0041
+ASCII_AT	.FILL x0040
+ASCII_NL	.FILL x000A 
+ASCII_SPACE	.FILL x0020
+
+R0_SAVE     .FILL 0
+R1_SAVE     .FILL 0
+R2_SAVE     .FILL 0
+R3_SAVE     .FILL 0
+R4_SAVE     .FILL 0
+R5_SAVE     .FILL 0
+R6_SAVE     .FILL 0
+R7_SAVE     .FILL 0
 
 ; for testing, you can use the lines below to include the string in this
 ; program...
