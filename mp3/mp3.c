223e01064d99fa4b754ba8102492e3db28952fa8
Samuel Hunag <lspss95207@gmail.com>
2021-09-16 11:14:29 -0500

    Finish mp3

--- mp3/mp3.c
+++ mp3/mp3.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-  int row;
 
-  printf("Enter a row index: ");
-  scanf("%d",&row);
+unsigned long long conbination(int n, int k);
 
-  // Write your code here
+int main() {
+    int row;
 
-  return 0;
+    printf("Enter a row index: ");
+    scanf("%d", &row);
+
+    // Write your code here
+
+    for(int i = 0;i <= row;i++){
+      unsigned long long result = conbination(row,i);
+      printf("%d ", result);
+    }
+
+    printf("\n");
+
+    return 0;
+}
+
+unsigned long long conbination(int n, int k) {
+    unsigned long long result = 1;
+    for (int i = 1; i <= k; i++) {
+        result *= (n + 1 - i);
+    }
+    for (int i = 1; i <= k; i++) {
+        result /= i;
+    }
+    return result;
 }
