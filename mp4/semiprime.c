152b07350a1a36de199b89fc353d97a9d956ad85
Samuel Hunag <lspss95207@gmail.com>
2021-09-23 11:18:05 -0500

    Finish mp4

--- mp4/semiprime.c
+++ mp4/semiprime.c
@@ -13,10 +13,10 @@ int is_prime(int number)
     if (number == 1) {return 0;}
     for (i = 2; i < number; i++) { //for each number smaller than it
         if (number % i == 0) { //check if the remainder is 0
-            return 1;
+            return 0;
         }
     }
-    return 0;
+    return 1;
 }
 
 
@@ -29,14 +29,16 @@ int print_semiprimes(int a, int b)
 {
     int i, j, k;
     int ret = 0;
-    for (i = a; i <=b; i++) { //for each item in interval
+    for (i = a; i <= b; i++) { //for each item in interval
         //check if semiprime
         for (j = 2; j <= i; j++) {
             if (i%j == 0) {
                 if (is_prime(j)) {
-                    k = i%j;
+                    k = i/j;
                     if (is_prime(k)) {
                         printf("%d ", i);
+                        ret = 1;
+                        break;
                     }
                 }
             }
