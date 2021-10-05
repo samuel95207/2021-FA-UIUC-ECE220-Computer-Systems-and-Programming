1de3aa5ef0b14d52f2a6db60839bc775549d23d3
Samuel Hunag <lspss95207@gmail.com>
2021-10-05 11:54:24 -0500

    Finish lab5

--- lab5/dice.c
+++ lab5/dice.c
@@ -1,5 +1,18 @@
 //Function for generating three d6 rolls
+#include <stdlib.h>
 
-void roll_three(int* one, int* two, int* three){
+void roll_three(int* one, int* two, int* three) {
+    *one = rand() % 6 + 1;
+    *two = rand() % 6 + 1;
+    *three = rand() % 6 + 1;
+}
 
+
+int count_dice(int* one, int* two, int* three){
+    if(*one == *two && *two == *three){
+        return 3;
+    }else if(*one == *two || *two == *three || *one == *three){
+        return 2;
+    }
+    return 1;
 }
--- lab5/main.c
+++ lab5/main.c
@@ -8,4 +8,34 @@
  * Otherwise print "\n"
  */
 
+#include <stdio.h>
+#include <stdlib.h>
 
+#include "dice.h"
+
+
+int main() {
+    int seed;
+
+    printf("Enter Seed:\n");
+    scanf("%d", &seed);
+
+    srand(seed);
+
+
+    int one, two, three;
+    roll_three(&one, &two, &three);
+
+    int countResult = count_dice(&one, &two, &three);
+    char* countResultStr = "";
+    if (countResult == 2) {
+        countResultStr = "Double!";
+    } else if (countResult == 3) {
+        countResultStr = "Triple!";
+    }
+
+    printf("%d %d %d %s\n", one, two, three, countResultStr);
+
+
+    return 0;
+}
