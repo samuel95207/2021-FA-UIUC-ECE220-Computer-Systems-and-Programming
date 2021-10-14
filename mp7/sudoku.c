540a6d5d62fb397eab6c1cf9f478bbe11b4d570c
Samuel Hunag <lspss95207@gmail.com>
2021-10-14 11:36:23 -0500

    Finish mp7

--- mp7/sudoku.c
+++ mp7/sudoku.c
@@ -8,10 +8,21 @@
 
 // Function: is_val_in_row
 // Return true if "val" already existed in ith row of array sudoku.
+
+struct pair{
+    int first;
+    int second;
+};
+
 int is_val_in_row(const int val, const int i, const int sudoku[9][9]) {
     assert(i >= 0 && i < 9);
 
     // BEG TODO
+    for (int idx = 0; idx < 9; idx++) {
+        if (sudoku[i][idx] == val) {
+            return 1;
+        }
+    }
 
     return 0;
     // END TODO
@@ -23,7 +34,11 @@ int is_val_in_col(const int val, const int j, const int sudoku[9][9]) {
     assert(j >= 0 && j < 9);
 
     // BEG TODO
-
+    for (int idx = 0; idx < 9; idx++) {
+        if (sudoku[idx][j] == val) {
+            return 1;
+        }
+    }
     return 0;
     // END TODO
 }
@@ -34,7 +49,18 @@ int is_val_in_3x3_zone(const int val, const int i, const int j, const int sudoku
     assert(i >= 0 && i < 9);
 
     // BEG TODO
+    int rowStart = i / 3 * 3;
+    int rowEnd = rowStart + 3;
+    int colStart = j / 3 * 3;
+    int colEnd = colStart + 3;
 
+    for (int row = rowStart; row < rowEnd; row++) {
+        for (int col = colStart; col < colEnd; col++) {
+            if (sudoku[row][col] == val) {
+                return 1;
+            }
+        }
+    }
     return 0;
     // END TODO
 }
@@ -45,15 +71,59 @@ int is_val_valid(const int val, const int i, const int j, const int sudoku[9][9]
     assert(i >= 0 && i < 9 && j >= 0 && j < 9);
 
     // BEG TODO
-    return 1;
+    return !sudoku[i][j] && !is_val_in_row(val, i, sudoku) && !is_val_in_col(val, j, sudoku) && !is_val_in_3x3_zone(val, i, j, sudoku);
     // END TODO
+}
+
+int is_sudoku_complete(const int sudoku[9][9]) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            if (sudoku[i][j] == 0) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+struct pair find_enpty_cell(const int sudoku[9][9]){
+    struct pair ij;
+    ij.first = -1;
+    ij.second = -1;
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            if (sudoku[i][j] == 0) {
+                ij.first = i;
+                ij.second = j;
+                return ij;
+            }
+        }
+    }
+    return ij;
 }
 
 // Procedure: solve_sudoku
 // Solve the given sudoku instance.
 int solve_sudoku(int sudoku[9][9]) {
     // BEG TODO.
+    int i, j;
+    if (is_sudoku_complete(sudoku)) {
+        return 1;
+    } else {
+        struct pair ij = find_enpty_cell(sudoku);
+        i = ij.first;
+        j = ij.second;
+    }
 
+    for (int num = 1; num <= 9; num++) {
+        if (is_val_valid(num, i, j, sudoku)) {
+            sudoku[i][j] = num;
+            if (solve_sudoku(sudoku)) {
+                return 1;
+            }
+            sudoku[i][j] = 0;
+        }
+    }
     return 0;
     // END TODO.
 }
