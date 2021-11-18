a314adc1986ff129bbb762228cd2ea74e41c254b
wu6u3 <tinyinlai@gmail.com>
2021-11-18 14:39:16 -0600

    mp12 and lab12

---
+++ mp12/shape.cpp
@@ -0,0 +1,79 @@
+#include "shape.hpp"
+
+
+
+//Base class
+//Please implement Shape's member functions
+//constructor, getName()
+//
+//Base class' constructor should be called in derived classes'
+//constructor to initizlize Shape's private variable 
+
+
+
+//Rectangle
+//Please implement the member functions of Rectangle:
+//constructor, getArea(), getVolume(), operator+, operator-
+//@@Insert your code here
+
+
+
+double Rectangle::getWidth()const{return width_;}
+double Rectangle::getLength()const{return length_;}
+
+
+//Circle
+//Please implement the member functions of Circle:
+//constructor, getArea(), getVolume(), operator+, operator-
+//@@Insert your code here
+
+double Circle::getRadius()const{return radius_;}
+
+//Sphere
+//Please implement the member functions of Sphere:
+//constructor, getArea(), getVolume(), operator+, operator-
+//@@Insert your code here
+
+
+
+double Sphere::getRadius()const{return radius_;}
+
+//Rectprism
+//Please implement the member functions of RectPrism:
+//constructor, getArea(), getVolume(), operator+, operator-
+//@@Insert your code here
+
+
+double RectPrism::getWidth()const{return width_;}
+double RectPrism::getHeight()const{return height_;}
+double RectPrism::getLength()const{return length_;}
+
+
+ 
+// Read shapes from test.txt and initialize the objects
+// Return a vector of pointers that points to the objects 
+vector<Shape*> CreateShapes(char* file_name){
+	//@@Insert your code here
+	return vector<Shape*>(0, NULL); // please remeber to modify this line to return the correct value
+}
+
+// call getArea() of each object 
+// return the max area
+double MaxArea(vector<Shape*> shapes){
+	double max_area = 0;
+	//@@Insert your code here
+	
+	return max_area;
+}
+
+
+// call getVolume() of each object 
+// return the max volume
+double MaxVolume(vector<Shape*> shapes){
+	double max_volume = 0;
+	//@@Insert your code here
+
+	
+	return max_volume;
+}
+
